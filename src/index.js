94468238bc794503dff06305985df488c15ca724
Yiran Yan <erin.yyr@gmail.com>
2018-08-09 00:48:30 -0700

    Ajax Requests with React

--- src/index.js
+++ src/index.js
@@ -1,17 +1,41 @@
-import React from 'react';
+import _ from 'lodash';
+import React, { Component } from 'react';
 import ReactDom from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search-bar';
-
+import VideoList from './components/video-list';
+import VideoDetail from './components/video-detail';
 const API_KEY = 'AIzaSyDoBOk8rPhTgKh46DdeNStsRi11CZEbAk8';
 
+
+
 // Create a new component. It should produce some HTML
-const App = () => {
-    return (
-    <div>
-        <SearchBar />
-    </div>
-    );
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { videos: [], selectedVideo: null };
+        this.videoSearch('minecraft');
+    }
+
+    videoSearch(term) {
+        YTSearch({ key: API_KEY, term: term }, (videos) => {
+            this.setState({ videos: videos, selectedVideo: videos[0] });
+        });
+    }
+
+    render() {
+        const vidoeSearch = _.debounce((term)=>{this.videoSearch(term)}, 300);
+
+        return (
+            <div>
+                <SearchBar onSearchTermChange={vidoeSearch} />
+                <VideoDetail video={this.state.selectedVideo} />
+                <VideoList
+                    videos={this.state.videos}
+                    onVideoSelect={selectedVideo => this.setState({ selectedVideo })} />
+            </div>
+        );
+    }
 }
 
-ReactDom.render(<App/>, document.querySelector('.container'));
+ReactDom.render(<App />, document.querySelector('.container'));
